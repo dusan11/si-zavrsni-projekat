cf5c1119a8fc9a065f58a82a5d86c7cd8fc3695f
Dusan Matkovic <matkovic.dusan99@gmail.com>
2022-06-10 22:14:09 +0200

    Added orderProduct crud functions

--- back-end/app/Http/Controllers/OrderController.php
+++ back-end/app/Http/Controllers/OrderController.php
@@ -19,5 +19,37 @@ class OrderController extends Controller
         return $data;
     }
 
-    
+    public function store(Request $request)
+    {
+        $data = Order::create($request->all());
+        return $data;
+    }
+
+    public function update(Request $request, $id)
+    {
+        $order = Order::find($id);
+        $order->update($request->all());
+        return $order;
+    }
+
+    public function destroy($id)
+    {
+        $order = Order::findOrFail($id);
+        $order->delete($id);
+        return'{"success":"You have succesfully deleted the order."}';
+    }
+
+    public function removeProduct($order_id, $product_id)
+    {
+        $order = Order::find($order_id);
+        $order->products()->detach($product_id);
+        return '{"success":"You have succesfully removed the order item."}';
+    }
+
+    public function addProduct(Request $request, $order_id, $product_id)
+    {
+        $order = Order::find($order_id);
+        $order->products()->attach($product_id, $request->input('amount'));
+        return '{"success":"You have succesfully added the order item."}';
+    }
 }
--- back-end/routes/api.php
+++ back-end/routes/api.php
@@ -3,6 +3,15 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\CompanyController;
+use App\Http\Controllers\CompanyTypeController;
+
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\OrderProductController;
+
+use App\Http\Controllers\OrderStatusController;
+use App\Http\Controllers\ProductController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +26,18 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resource('company', CompanyController::class);
+Route::resource('company-type', CompanyTypeController::class);
+Route::resource('order-status', OrderStatusController::class);
+Route::resource('product', ProductController::class);
+
+Route::get('order', [OrderController::class, 'index']);
+Route::get('order/{id}', [OrderController::class, 'show']);
+Route::post('order', [OrderController::class, 'store']);
+Route::put('order/{id}', [OrderController::class, 'update']);
+Route::delete('order/{id}', [OrderController::class, 'destroy']);
+
+Route::post('order/{order_id}/product/{product_id}', [OrderController::class, 'addProduct']);
+Route::delete('order/{order_id}/product/{product_id}', [OrderController::class, 'removeProduct']);
+
