d7daf625e97050217503e3512997d18b9b4b76a8
Dusan Matkovic <matkovic.dusan99@gmail.com>
2022-06-16 22:50:19 +0200

    Forms for login and register and validation for register

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,14 +1,37 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { HomeComponent } from './home/home.component';
+
+const routes:Routes = [
+  {
+    path: '', component:HomeComponent
+  },
+  {
+    path: 'register', component:RegisterComponent
+  },
+  {
+    path: 'login', component:LoginComponent
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes),
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ front-end/src/app/register/register.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+  form!: FormGroup;
+  submitted = false;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  createForm(){
+    this.form = this.formBuilder.group({
+      name: [null, Validators.required],
+      email: ['', Validators.required, Validators.email],
+      password: ['', Validators.required, Validators.minLength(4)],
+    })
+  }
+  ngOnInit(): void {
+    this.createForm();
+  }
+
+  get f(){
+    return this.form.controls;
+  }
+
+  submit(){
+    this.submitted = true;
+    if(this.form.invalid){
+      return;
+    }
+  }
+
+}
