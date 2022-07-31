47caf4861ef53068a0fc4798f769c4df9d978132
Dusan Matkovic <matkovic.dusan99@gmail.com>
2022-07-31 12:39:33 +0200

    modified functions

--- back-end/app/Http/Controllers/CompanyController.php
+++ back-end/app/Http/Controllers/CompanyController.php
@@ -3,38 +3,57 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Utakmica;
+use App\Models\Company;
 
 class CompanyController extends Controller
 {
     public function index() 
     {
-        $data = Company::with('type')->get();
+        $data = Company::with('company_type')->get();
         return $data;
     }
 
     public function store(Request $request)
     {
-        $data = Company::create($request->all());
+    //    $data = Company::with('company_type')->create($request->all());
+
+        $success = Company::insert([
+            'name' => $request->name,
+            'adress' => $request->adress,
+            'account_no' => $request->account_no,
+            'email' => $request->email,
+            'responsible_person' => $request->responsible_person,
+            'company_type_id' => $request->company_type
+        ]);
+
         $respData['status']=201;
         $respData['message']='Successfully created.';
-        $respData['data']=$data;
+        $respData['success']=$success;
         return response()->json($respData);
     }
 
     public function show($id)
     {
-        $data = Company::with('type')->findOrFail($id);
+        $data = Company::with('company_type')->findOrFail($id);
         return $data;
     }
 
     public function update(Request $request, $id)
     {
         $company = Company::find($id);
-        $company->update($request->all());
+  //    $company->update($request->all());
+
+        $company->update([
+            'name' => $request->name,
+            'adress' => $request->adress,
+            'account_no' => $request->account_no,
+            'email' => $request->email,
+            'responsible_person' => $request->responsible_person,
+            'company_type_id' => $request->company_type
+        ]);
         $respData['status']=204;
         $respData['message']='Successfully updated.';
-        $respData['data']=$company;
+        $respData['success']=$company;
         return response()->json($respData);
     }
 
--- back-end/app/Models/Company.php
+++ back-end/app/Models/Company.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\CompanyType;
 
 class Company extends Model
 {
@@ -16,7 +17,7 @@ class Company extends Model
 
     ];
 
-    public function type()
+    public function company_type()
     {
         return $this->belongsTo(CompanyType::class);
     }
